12966498767781abe3e764a7ce3c70294e0b73a8
hai-henry <hvnryho@gmail.com>
2024-12-17 07:52:19 -0600

    Added Prediction

--- 1-Introduction/classify_iris.py
+++ 1-Introduction/classify_iris.py
@@ -8,42 +8,38 @@ the measurements and we want to predict the species which makes this a
 classification problem. The different species are called classes.
 """
 
-from sklearn.datasets import load_iris  # Import sklearn dataset
-from sklearn.model_selection import train_test_split  # Dataset split function
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
+import numpy as np
 import mglearn
 import matplotlib.pyplot as plt
 
 
-def print_data_info(dataset):
-    # Keys of the dataset
-    # print("Keys of iris_dataset: \n{}".format(iris_dataset.keys()))
-
-    # Take the key (target_names) and print the targets (setosa, versicolor,
-    # virginica)
-    # print("Target names: {}".format(iris_dataset['target_names']))
-
-    # Print features (sepal (len & wid, petal (len & wid)))
-    # print("Feature names: {}".format(iris_dataset['feature_names']))
-
-    # Print features, target
-    for features, target in zip(dataset["data"], dataset["target"]):
-        print(f" {features}, {target}")
-
-
 def pair_plot_data(dataset, X_train, y_train):
-    # Convert X_train to dataframe
-    # Columns are labeled with features
+    """
+    Creates a pair scatter plot matrix for training data.
+
+    Patamteters:
+        dataset (dict): A dictionary containing feature data and target labels.
+        X_train: Training features.
+        y_train: Training target.
+
+    Returns:
+        plot: Pair scatter matrix
+    """
+    # Convert X_train to dataframe, with columns labeled with features
     iris_dataframe = pd.DataFrame(X_train, columns=dataset.feature_names)
 
-    # Create scatter matrix from dataframe, color by y_train (output)
+    # Create scatter matrix from dataframe, coloring by y_train (output)
     pd.plotting.scatter_matrix(
         iris_dataframe,
         c=y_train,
         figsize=(15, 15),
         marker="o",
-        hist_kwds={"bins": 20},
         s=60,
+        hist_kwds={"bins": 20},
         alpha=0.8,
         cmap=mglearn.cm3,
     )
@@ -51,15 +47,43 @@ def pair_plot_data(dataset, X_train, y_train):
 
 
 def main():
-    iris_dataset = load_iris()  # Load the dataset to variable
+    # Load iris dataset
+    iris_dataset = load_iris()
 
-    # Split data into training and test set (75%, 25%)
-    # Fix seed random_state = 0, so it does not shuffle
+    # Split data (NumPy array) into training and test set (75% train, 25% test)
+    # Fix seed random_state = 0, ensure same split on runs
     X_train, X_test, y_train, y_test = train_test_split(
         iris_dataset["data"], iris_dataset["target"], random_state=0
     )
 
+    # Visualize training set
     pair_plot_data(iris_dataset, X_train, y_train)
+
+    # Initialize KNN model and train (k = 1, single nearest neighbor)
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(X_train, y_train)
+
+    # New data point we want to classify
+    # [sepal length (cm)], sepal width (cm), petal length (cm), petal width (cm)
+    X_new = np.array([[5, 2.9, 1, 0.2]])
+
+    # Predict class with new data point
+    prediction = knn.predict(X_new)
+    print("Prediction: ", prediction)
+    print("Prediction target: ", iris_dataset["target_names"][prediction])
+
+    # Evaluating model for accuracy (correct species predicted) using test data
+    y_predict = knn.predict(X_test)  # Makes predicitons with X_test features
+    print("Test set prediction: \n", y_predict)  # Test predictions targets
+    print("Test set targets: \n", y_test)  # Test actual targets
+
+    # Print accracy two decimal points
+    # knn.score = accuracy = (# of correct predictions)/(Total # of test samples)
+    print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
+
+    # Calculate accuracy manually
+    accuracy = np.mean(y_predict == y_test)
+    print("Manual accuracy: {:.2f}".format(accuracy))
 
 
 if __name__ == "__main__":
